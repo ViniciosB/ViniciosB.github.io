3055e7df380a48022450b5010fb72b683a54211f
viniciosb <viny1841@gmail.com>
2021-07-08 01:25:02 -0300

    Add files via upload

---
+++ RMI/CalculadoraServidor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CalculadoraServidor{
+	
+	public static void main(String[] args) {
+		Scanner cc=new Scanner(System.in);
+			System.out.println("Servidor online");
+			int n=cc.nextInt();
+		
+	}
+
+}
